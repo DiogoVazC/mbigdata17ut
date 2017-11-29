d0d85b1325de1905ca7e23c41544402436eeab2c
Diogo Carvalho <diogoavc@gmail.com>
2017-11-29 17:30:01 +0100

    Finished MR-DSORT

--- challenges/MapReduce-code/MR-DSORT-s1997319-s2000032-stritnaightz.py
+++ challenges/MapReduce-code/MR-DSORT-s1997319-s2000032-stritnaightz.py
@@ -5,12 +5,23 @@ s2000032 - Diogo Antunes Vaz de Carvalho
 import MapReduce
 
 def mapper(key, value):
-
-    for key in acc:
-        mr.emit_intermediate(key, acc[key])
+    '''
+    Sorting partially sorted lists is much more efficient
+    in terms of operations and memory consumption
+    than sorting the complete list.
+    As each batch of the input has the same length,
+    one can consider each batch as one equally-sized sublist to be sorted in the mapper
+    '''
+    value.sort()
+    mr.emit_intermediate(0, value)
 
 def reducer(key, list_of_values):
-    mr.emit((key, sum(list_of_values)))
+    # Flatten lists received from mapper
+    numList = [val for sublist in list_of_values for val in sublist]
+    # Sort the whole list. This will be more efficient with previously sorted sublists
+    numList.sort()
+    for num in numList:
+        mr.emit(num)
 
 
 if __name__ == '__main__':
