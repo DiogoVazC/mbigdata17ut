1184a77b4dc9667066da1af994d9b242a2b078d2
Diogo Carvalho <diogoavc@gmail.com>
2017-12-01 18:38:05 +0100

    added names and random strings

--- challenges/Spark-code/USERS-s1997319-s2000032-usserrsteewtsds.py
+++ challenges/Spark-code/USERS-s1997319-s2000032-usserrsteewtsds.py
@@ -1,25 +1,30 @@
-"""
-This computes the most frequent 20 hashtags across the 
+'''
+s1997319 - Joao David Goncalves Baiao
+s2000032 - Diogo Antunes Vaz de Carvalho
+
+----------------------------------------------
+
+This computes the most frequent 40 tweeters across the
 tweets in the HDFS file /data/doina/twitterNL/201612/20161231-23.out.gz
 
 To execute on a Farm machine:
-time spark-submit HASHTAGS-s1997319-s2000032-tweets.py 2> /dev/null
-"""
+time spark-submit USERS-s1997319-s2000032-tweets.py 2> /dev/null
+'''
 
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 sc = SparkContext("local", "Twitter")
 sqlc = SQLContext(sc)
 df = sqlc.read.json("/data/doina/twitterNL/201612/20161231-23.out.gz")
-tweets = df.select("entities.hashtags.text")
+tweets = df.select("user.screen_name")
 tweets.printSchema()
 
 top_tweets = tweets \
-	.flatMap(lambda contents: contents.text) \
+	.flatMap(lambda contents: contents) \
 	.map(lambda word: (word, 1)) \
 	.reduceByKey(lambda a, b: a+b) \
 	.sortBy(lambda record: record[1], ascending=False) \
-	.take(20)
+	.take(40)
 
 for (w, c) in top_tweets:
-	print "Hashtag:\t", w, "\toccurrences:\t", c
+	print "User:\t", w, "\toccurrences:\t", c
