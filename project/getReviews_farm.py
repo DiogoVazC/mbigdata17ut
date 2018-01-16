4e743c574ac8199e15465fe1907f40ae6cdb201b
João Baião <joaobaiao12@gmail.com>
2018-01-16 17:48:02 +0100

    Farm module functional

--- project/getReviews_farm.py
+++ project/getReviews_farm.py
@@ -6,33 +6,34 @@ To execute on a Farm machine:
 time spark-submit getReviews_farm.py [user] [company] 2> /dev/null
 """
 
-import sys
-user = sys.argv[1]
-company = sys.argv[2]
-beginTime = sys.argv[3] if (len(sys.argv) > 4) else 1356998400
-endTime = sys.argv[4] if (len(sys.argv) > 4) else 1388534399
-
+"""Import packages"""
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
-from pyspark.sql.functions import approxCountDistinct, countDistinct, col, avg
+from pyspark.sql.functions import col, avg, to_date, from_unixtime
+import dataframeOperations as operation
+import printResults as printR
+import consts
 
+"""Get Arguments"""
+import sys
+company = sys.argv[1]
+beginTime = sys.argv[2] if (len(sys.argv) > 3) else consts.Jan2013 
+endTime = sys.argv[3] if (len(sys.argv) > 3) else consts.Jun2013 
 
-metaFile = '/data/doina/UCSD-Amazon-Data/meta_Electronics.json.gz'
-reviewsfile = 'file:///home/' + user + '/reviews_Electronics.json.gz'
-
-sc = SparkContext("local", "AmazonReviews")
+"""Initialize Spark"""
+sc = SparkContext(appName="Amazon Products")
 sqlc = SQLContext(sc)
 
-df = sqlc.read.json(metaFile)
-products = df.select("asin", "title", "price")
-meta = products.filter(products.title.rlike('(?i).*' + company + '.*')) 	\
-	.filter(products.price > 50)
+"""Read Files"""
+df = sqlc.read.json(consts.filename)
+df2 = sqlc.read.json(consts.reviewsfilefarm)
 
-df2 = sqlc.read.json(reviewsfile)
-reviews = df2.select('asin', "overall", "summary", "unixReviewTime", "reviewTime") \
-	.filter(df2.unixReviewTime > beginTime) \
-	.filter(df2.unixReviewTime < endTime)
+"""Select Data"""
+meta = operation.selectProducts(df, ["asin", "title", "price"], company, 50)
+reviews = operation.selectReviews(df2, ['asin', "overall", "summary", "unixReviewTime", "reviewTime"], beginTime, endTime)
 
+"""Join"""
 reviews = reviews.join(meta, "asin")
 
-print reviews.take(10)
+"""Print"""
+printR.printFarmExample(reviews, 10)
--- project/selects.py
+++
@@ -1,5 +0,0 @@
-def selectProducts(dataframe, col, title, price):
-	products = dataframe.select(col)
-	meta = products.filter(products.title.rlike('(?i).*' + title + '.*')) 	\
-		.filter(products.price > price)
-	return meta
